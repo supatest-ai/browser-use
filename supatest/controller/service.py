219840503ca1c71f4181d267c3d509474a9b62a0
Devang Vishnu <thedevangvishnu@gmail.com>
2025-02-28 20:09:59 +0530

    Remove extract_conent from supatest controller

--- supatest/controller/service.py
+++ supatest/controller/service.py
@@ -190,26 +190,6 @@ class SupatestController(Controller[Context]):
             logger.info(msg)
             return ActionResult(extracted_content=msg, include_in_memory=True)
 
-        @self.registry.action('Extract page content to retrieve specific information from the page, e.g. all company names, a specifc description, all information about, links with companies in structured format or simply links')
-        async def extract_content(goal: str, browser: SupatestBrowserContext, page_extraction_llm: BaseChatModel):
-            page = await browser.get_current_page()
-            import markdownify
-
-            content = markdownify.markdownify(await page.content())
-
-            prompt = 'Your task is to extract the content of the page. You will be given a page and a goal and you should extract all relevant information around this goal from the page. If the goal is vague, summarize the page. Respond in json format. Extraction goal: {goal}, Page: {page}'
-            template = PromptTemplate(input_variables=['goal', 'page'], template=prompt)
-            try:
-                output = page_extraction_llm.invoke(template.format(goal=goal, page=content))
-                msg = f'📄  Extracted from page\n: {output.content}\n'
-                logger.info(msg)
-                return ActionResult(extracted_content=msg, include_in_memory=True)
-            except Exception as e:
-                logger.debug(f'Error extracting content: {e}')
-                msg = f'📄  Extracted from page\n: {content}\n'
-                logger.info(msg)
-                return ActionResult(extracted_content=msg)
-
         @self.registry.action('Scroll down the page by pixel amount - if no amount is specified, scroll down one page', param_model=ScrollAction)
         async def scroll_down(params: ScrollAction, browser: SupatestBrowserContext):
             page = await browser.get_current_page()
